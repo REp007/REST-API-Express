ef8c75122b006d2625d3ec76d139ca21ee7a29fd
EL-Amrani Youssef <elamraniy292@gmail.com>
2024-04-06 09:27:04 +0000

    config the app server

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+// eslint-disable-next-line
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`start listening at: ${port}`)
+  console.log(`http://127.0.0.1:${port}/`)
+})
