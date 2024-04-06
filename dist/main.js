1006c4c40ba96cb4f4d04d6dc596485676eba46c
EL-Amrani Youssef <elamraniy292@gmail.com>
2024-04-06 09:44:41 +0000

    add GET Methods

--- dist/main.js
+++ dist/main.js
@@ -6,3 +6,7 @@ app.listen(port, () => {
     console.log(`start listening at: ${port}`);
     console.log(`http://127.0.0.1:${port}/`);
 });
+app.get('/', (req, res) => {
+    console.log('GET Request syccessfull!');
+    res.send('GET Req successfully init');
+});
