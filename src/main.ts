f7bb41281d2885297613cc74a21ef6c4f9b5bc5f
EL-Amrani Youssef <elamraniy292@gmail.com>
2024-04-06 09:48:16 +0000

    add POST Method

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,19 @@ const app = express()
 
 // eslint-disable-next-line
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`start listening at: ${port}`)
-  console.log(`http://127.0.0.1:${port}/`)
-})
 
 app.get('/', (req, res) => {
   console.log('GET Request syccessfull!')
   res.send('GET Req successfully init')
-});
+})
 
+app.post('/', (req, res) => {
+  console.log('POST Request successful')
+  console.log(req.body)
+  res.send('Data POST Request Recieved')
+})
 
+app.listen(port, () => {
+  console.log(`start listening at: ${port}`)
+  console.log(`http://127.0.0.1:${port}/`)
+})
